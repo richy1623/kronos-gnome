cf7aedd82d341df4ffe188f7a36d5e025346f754
Richard Paterson <richy1623@gmail.com>
2026-06-28 18:17:09 +0200

    Using blueprints

--- src/application.rs
+++ src/application.rs
@@ -1,11 +1,10 @@
-
-use gettextrs::gettext;
 use adw::prelude::*;
 use adw::subclass::prelude::*;
+use gettextrs::gettext;
 use gtk::{gio, glib};
 
-use crate::config::VERSION;
 use crate::KronosWindow;
+use crate::config::VERSION;
 
 mod imp {
     use super::*;
@@ -26,6 +25,7 @@ mod imp {
             let obj = self.obj();
             obj.setup_gactions();
             obj.set_accels_for_action("app.quit", &["<control>q"]);
+            obj.set_accels_for_action("app.shortcuts", &["<control>comma"]);
         }
     }
 
@@ -73,7 +73,11 @@ impl KronosApplication {
         let about_action = gio::ActionEntry::builder("about")
             .activate(move |app: &Self, _, _| app.show_about())
             .build();
-        self.add_action_entries([quit_action, about_action]);
+        let shortcuts_action = gio::ActionEntry::builder("shortcuts")
+            .activate(move |app: &Self, _, _| app.show_shortcuts())
+            .build();
+
+        self.add_action_entries([quit_action, about_action, shortcuts_action]);
     }
 
     fn show_about(&self) {
@@ -90,5 +94,17 @@ impl KronosApplication {
             .build();
 
         about.present(Some(&window));
+    }
+
+    fn show_shortcuts(&self) {
+        let window = self.active_window().unwrap();
+
+        let builder = gtk::Builder::from_resource("/io/richard/kronos/shortcuts_dialog.ui");
+
+        let shortcuts_dialog: adw::ShortcutsDialog = builder
+            .object("shortcuts_dialog")
+            .expect("Could not find shortcuts_dialog ID in layout");
+
+        shortcuts_dialog.present(Some(&window));
     }
 }
--- src/window.rs
+++ src/window.rs
@@ -8,7 +8,7 @@ mod imp {
     #[derive(Debug, Default, gtk::CompositeTemplate)]
     #[template(resource = "/io/richard/kronos/window.ui")]
     pub struct KronosWindow {
-        // Template widgets
+        // This variable matches the name "label" from your blueprint file perfectly!
         #[template_child]
         pub label: TemplateChild<gtk::Label>,
     }
@@ -28,7 +28,16 @@ mod imp {
         }
     }
 
-    impl ObjectImpl for KronosWindow {}
+    impl ObjectImpl for KronosWindow {
+        // This lifecycle hook triggers as soon as the window and its UI are fully constructed
+        fn constructed(&self) {
+            self.parent_constructed();
+
+            // Fire up our custom UI logic
+            self.obj().setup_internal_logic();
+        }
+    }
+
     impl WidgetImpl for KronosWindow {}
     impl WindowImpl for KronosWindow {}
     impl ApplicationWindowImpl for KronosWindow {}
@@ -36,9 +45,10 @@ mod imp {
 }
 
 glib::wrapper! {
-pub struct KronosWindow(ObjectSubclass<imp::KronosWindow>)
-    @extends gtk::Widget, gtk::Window, gtk::ApplicationWindow, adw::ApplicationWindow,
-    @implements gio::ActionGroup, gio::ActionMap, gtk::Accessible, gtk::Buildable, gtk::ConstraintTarget, gtk::Native, gtk::Root, gtk::ShortcutManager;
+    pub struct KronosWindow(ObjectSubclass<imp::KronosWindow>)
+        @extends gtk::Widget, gtk::Window, gtk::ApplicationWindow, adw::ApplicationWindow,
+        @implements gio::ActionGroup, gio::ActionMap, gtk::Accessible, gtk::Buildable,
+                    gtk::ConstraintTarget, gtk::Native, gtk::Root, gtk::ShortcutManager;
 }
 
 impl KronosWindow {
@@ -46,5 +56,17 @@ impl KronosWindow {
         glib::Object::builder()
             .property("application", application)
             .build()
+    }
+
+    // This is where you write your interactive features for the window!
+    fn setup_internal_logic(&self) {
+        // 1. Get access to the private UI struct layer
+        let imp = self.imp();
+
+        // 2. Fetch the actual native GtkLabel instance
+        let main_label = imp.label.get();
+
+        // 3. You can now modify it dynamically from Rust!
+        main_label.set_label("Hello from the updated Rust window!");
     }
 }
